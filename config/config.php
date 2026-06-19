953e9478c541a9157163a30dcecf109222ca9d28
hanhhle <hoanganhle03201208@gmail.com>
2026-06-19 12:23:44 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST',          'localhost');
+define('DB_NAME',          'web2');
+define('DB_USER',          'root');
+define('DB_PASS',          '');
+define('DB_CHARSET',       'utf8mb4');
+define('APP_NAME',         'VNU-IS Grading System');
+define('SEMESTER', '2025-1');
+define('SEMESTER_CURRENT', '2025-1');
+
